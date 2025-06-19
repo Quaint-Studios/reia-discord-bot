741c6db4b3ecccca567200a266050739f7aab1f4
Makosai <kristopher.ali.dev@gmail.com>
2025-06-19 17:50:47 -0400

    Initialize bot.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+import { SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
+
+const client = new SapphireClient({
+  intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  loadMessageCommandListeners: true
+});
+
+client.login(process.env.BOT_TOKEN);
